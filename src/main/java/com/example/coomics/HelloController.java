a9c394c5b95db2c40c2ff0ed7c941a6e6291c8a9
Giulio <giulio.appetito.ga@gmail.com>
2021-10-24 17:28:12 +0200

    Giulio1

--- src/main/java/com/example/coomics/HelloController.java
+++ src/main/java/com/example/coomics/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Ciao Anastasia!");
+        welcomeText.setText("Ciao Giulio!");
     }
 }
