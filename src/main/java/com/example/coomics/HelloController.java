ecbf585800c2825318d624c297da2a90ad73a692
Anastasia <anastasia.brinati@students.uniroma2.eu>
2021-10-24 17:26:28 +0200

    commit anastasia

--- src/main/java/com/example/coomics/HelloController.java
+++ src/main/java/com/example/coomics/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Ciao Giulio!");
+        welcomeText.setText("Ciao Anastasia!");
     }
 }
