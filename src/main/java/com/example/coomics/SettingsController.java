7d68b769b09f057be3ada38c7cf9c9582f8361c5
Anastasia <anastasia.brinati@students.uniroma2.eu>
2021-10-25 09:40:35 +0200

    adding settings

---
+++ src/main/java/com/example/coomics/SettingsController.java
@@ -0,0 +1,31 @@
+package com.example.coomics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class SettingsController {
+
+    @FXML
+    private Button profile;
+    @FXML
+    private VBox settingsBox;
+    @FXML
+    private BorderPane mainPane;
+    @FXML
+    private Button accountsettings;
+    @FXML
+    private Button preferencessettings;
+    @FXML
+    private Button badgessettings;
+    @FXML
+    private Button aboutussettings;
+
+    public void openProfile(MouseEvent mouseEvent) {
+    }
+
+    public void openAccount(MouseEvent mouseEvent) {
+    }
+}
