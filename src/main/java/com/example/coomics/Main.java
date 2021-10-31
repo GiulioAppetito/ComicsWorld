6b537f4920a9a9ddf8f154a9b4d426b710ef5825
Anastasia <anastasia.brinati@students.uniroma2.eu>
2021-10-31 12:06:50 +0100

    login try

--- src/main/java/com/example/coomics/Main.java
+++ src/main/java/com/example/coomics/Main.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        stage.setTitle("ComicsWorld");
-        stage.setScene(scene);
-        stage.show();
+        LoginController.getInstance(stage);
     }
 
     public static void main(String[] args) {
