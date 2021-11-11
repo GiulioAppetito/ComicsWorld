8a61e961cd630214ce1e0bbd1a1b2fc4e5704277
Giulio <giulio.appetito.ga@gmail.com>
2021-11-11 11:49:05 +0100

    review editor added

--- src/main/java/com/example/comics/view1/ChapterControllerG.java
+++ src/main/java/com/example/comics/view1/ChapterControllerG.java
@@ -2,12 +2,16 @@ package com.example.comics.view1;
 
 import com.example.comics.model.Advertisement;
 import com.example.comics.model.Review;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ChapterControllerG {
@@ -15,7 +19,14 @@ public class ChapterControllerG {
     @FXML
     private VBox vbReviews;
 
+
+    @FXML
+    private Pane paneInsertReview;
+
     public void init(){
+
+        paneInsertReview.setVisible(false);
+
         ArrayList<Review> listOfReviews = new ArrayList<>(addReviews());
         int len = listOfReviews.size();
 
@@ -42,13 +53,15 @@ public class ChapterControllerG {
 
         List<Review> lr = new ArrayList<>();
         int i;
-        int n = 8;
+        List<String> nameList = Arrays.asList("Anastasia","Giulio","Gregor","Peter");
+        List<String> commentList = Arrays.asList("Very interesting chapter!","I didn't like this chapter.","Not my favourite chapter...","Best chapter ever!");
 
 
-        for(i=0; i<n; i++){
+
+        for(i=0; i<nameList.size(); i++){
             Review review = new Review();
-            review.setComment("Comment n°"+ i);
-            review.setUsername("Anastasia");
+            review.setComment(commentList.get(i));
+            review.setUsername(nameList.get(i));
             //comic.setImageSrc(null);
             lr.add(review);
         }
@@ -57,4 +70,18 @@ public class ChapterControllerG {
         return lr;
 
     }
+
+    public void openEditor(){
+        paneInsertReview.setVisible(true);
+    }
+
+    @FXML
+    private Button btnCloseEditor;
+
+    @FXML
+    void closeEditor() {
+        paneInsertReview.setVisible(false);
+
+    }
+
 }
