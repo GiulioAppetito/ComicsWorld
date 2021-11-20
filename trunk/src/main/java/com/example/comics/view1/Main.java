7b1870a3b1f232ab32349aa6ebf9f7ea392a17a3
Anastasia <anastasia.brinati@students.uniroma2.eu>
2021-11-20 17:50:19 +0100

    sonar

--- trunk/src/main/java/com/example/comics/view1/Main.java
+++ trunk/src/main/java/com/example/comics/view1/Main.java
@@ -26,15 +26,6 @@ public class Main extends Application {
         stage.setTitle("ComicsWorld");
         stage.setScene(scene);
         stage.show();
-        int k = 0;
-        int a = 1;
-
-
-        //senti ora andrà ritoccato tutto perché:
-        //non possiamo/dobbiamo più associare i controllori e i metodi da scene builder, ma metterli a mano
-        //quindi per i bottoni già esistenti etc, dobbiamo farlo alla creazione delle istanze nei costruttori di ognuno
-        //good luck a te del futuro
-        //però funziona capisci sarà tutto singleton
 
     }
 
