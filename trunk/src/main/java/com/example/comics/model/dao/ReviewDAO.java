62b6c23f320717f8af88aa18ccd1bf2f0a0213d7
Anastasia <anastasia.brinati@students.uniroma2.eu>
2022-01-09 21:13:17 +0100

    problemi

--- trunk/src/main/java/com/example/comics/model/dao/ReviewDAO.java
+++ trunk/src/main/java/com/example/comics/model/dao/ReviewDAO.java
@@ -25,7 +25,7 @@ public class ReviewDAO {
             ResultSet rs = Queries.retreiveReviewsByChapter(stmt, chapter);
 
             if(!rs.first()){
-                throw new ReviewsNotFoundException();
+                return reviewsList;
             }
             rs.first();
             do{
