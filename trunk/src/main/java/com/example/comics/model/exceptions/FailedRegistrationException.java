88c90acffdc6124a165894a8a67c41bc8e61e912
Giulio <giulio.appetito.ga@gmail.com>
2022-01-13 09:01:16 +0100

    FailedLoginException and FailedRegistrationException added

---
+++ trunk/src/main/java/com/example/comics/model/exceptions/FailedRegistrationException.java
@@ -0,0 +1,17 @@
+package com.example.comics.model.exceptions;
+
+public class FailedRegistrationException extends Exception{
+    private static final long serialVersionUID = 2L;
+
+    public FailedRegistrationException (String message){
+        super(message);
+    }
+
+    public FailedRegistrationException (Throwable cause) {
+        super(cause);
+    }
+
+    public FailedRegistrationException (String message, Throwable cause) {
+        super(" +++ " + message + " +++ ", cause);
+    }
+}
