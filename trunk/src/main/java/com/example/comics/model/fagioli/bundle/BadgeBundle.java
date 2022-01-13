e8915b6cac37069d4d893ee3b9bf100e45a9d96b
Giulio <giulio.appetito.ga@gmail.com>
2022-01-13 13:43:16 +0100

    badges

--- trunk/src/main/java/com/example/comics/model/fagioli/bundle/BadgeBundle.java
+++ trunk/src/main/java/com/example/comics/model/fagioli/bundle/BadgeBundle.java
@@ -4,23 +4,27 @@ import com.example.comics.model.fagioli.BadgeBean;
 import javafx.scene.image.Image;
 
 public class BadgeBundle implements BadgeBean {
+
+    private String name;
+    private Image icon;
+
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public void setName(String name) {
-
+        this.name = name;
     }
 
     @Override
     public Image getIcon() {
-        return null;
+        return this.icon;
     }
 
     @Override
     public void setIcon(Image icon) {
-
+        this.icon = icon;
     }
 }
