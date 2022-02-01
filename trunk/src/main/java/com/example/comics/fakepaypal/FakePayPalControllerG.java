56380b82bc66edfaf07daed4e031c67d5d5d623a
Anastasia <anastasia.brinati@students.uniroma2.eu>
2022-02-01 17:49:00 +0100

    code smells

--- trunk/src/main/java/com/example/comics/fakepaypal/FakePayPalControllerG.java
+++ trunk/src/main/java/com/example/comics/fakepaypal/FakePayPalControllerG.java
@@ -1,37 +1,66 @@
 package com.example.comics.fakepaypal;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class FakePayPalControllerG {
 
     @FXML
-    private Label lblFirstName;
+    private Button btnBuy;
 
     @FXML
-    private Label lblLastName;
+    private Button btnClose;
+
+    @FXML
+    private Button btnCloseErrorPane;
+
+    @FXML
+    private TextField tfFirstName;
+
+    @FXML
+    private Pane errorPane;
+
+    @FXML
+    private Label lblPrice;
 
     @FXML
     private TextField tfCard;
 
     @FXML
-    private Button btnConvalidate;
+    private TextField tfLastName;
 
     public FakePayPalControllerG(String firstName, String lastName){
-        lblFirstName.setText(firstName);
-        lblLastName.setText(lastName);
-        btnConvalidate.setOnAction(event -> convalidateCard());
+        errorPane.setVisible(false);
+        tfFirstName.setText(firstName);
+        tfLastName.setText(lastName);
+        btnBuy.setOnAction(event -> convalidateCard());
+        btnClose.setOnAction(event -> close(event));
+        btnCloseErrorPane.setOnAction(event -> closeErrorPane());
     }
+
+    private void close(ActionEvent event) {
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
+    }
+
+    private void closeErrorPane() {
+        errorPane.setVisible(false);
+    }
+
+
 
     private boolean convalidateCard() {
         String cardID = tfCard.getText();
         FakePayPal fakePayPal = new FakePayPal();
         return fakePayPal.isCardValid(cardID);
     }
+
 
 
 }
