de0fd3549e8b418fa9d6a19480430165ef25cd72
Giulio <giulio.appetito.ga@gmail.com>
2022-02-09 11:21:19 +0100

    selenium fixed

--- trunk/src/test/java/selenium/TestSelenium.java
+++ trunk/src/test/java/selenium/TestSelenium.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestSelenium1 {
+public class TestSelenium {
     @Test
     public void testCase(){
         System.setProperty("webdriver.chrome.driver","trunk/src/test/java/selenium/Driver/chromedriver.exe");
