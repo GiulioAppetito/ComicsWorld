6399bedf18720016d44d444495372e96bc1b5053
Anastasia <anastasia.brinati@students.uniroma2.eu>
2022-02-11 23:28:13 +0100

    tests

--- trunk/src/test/java/selenium/ide/TestInuyashaAuthor.java
+++ trunk/src/test/java/selenium/ide/TestInuyashaAuthor.java
@@ -4,27 +4,15 @@ import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 public class TestInuyashaAuthor {
     private WebDriver driver;
-    private Map<String, Object> vars;
     JavascriptExecutor js;
     @Before
     public void setUp() {
@@ -32,7 +20,6 @@ public class TestInuyashaAuthor {
 
         driver = new ChromeDriver();
         js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
     }
     @After
     public void tearDown() {
