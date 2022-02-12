aa3d29deffe309d5381b79ebb384592497a550eb
Giulio <giulio.appetito.ga@gmail.com>
2022-02-12 16:17:46 +0100

    fixed

--- trunk/src/logic/java/com/example/comics/controller/boundaries/MarkChapterAsReadReaderBoundary.java
+++ trunk/src/logic/java/com/example/comics/controller/boundaries/MarkChapterAsReadReaderBoundary.java
@@ -13,39 +13,16 @@ import javax.mail.internet.MimeMessage;
 import static com.example.comics.controller.boundaries.MailProperties.FROM;
 import static com.example.comics.controller.boundaries.MailProperties.initializeProperties;
 
-public class MarkChapterAsReadReaderBoundary {
+public class MarkChapterAsReadReaderBoundary extends MailProperties{
     public void sendEmailForDiscountCode(AccountBean readerBean, SeriesBean seriesBean, DiscountCodeBean discountCodeBean) {
 
-        // Recipient's email ID needs to be mentioned.
         String to = readerBean.getEmail();
+        String s = "New discount code just received!";
+        String m = "Hello from ComicsWorld! Your read chapters just allowed you to receive a new badge! Check it out on your profile" +
+                "Use the following code " + discountCodeBean.getCode() + " to purchase from " + seriesBean.getTitle() + ", with a " + discountCodeBean.getDiscountBean().getPercentage() +
+                "% off (up to " + discountCodeBean.getDiscountBean().getLimitDays() + " days)";
 
-        // Get the default Session object.
-        Session session = initializeProperties();
-
-        try {
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(FROM));
-
-            // Set To: header field of the header.
-            message.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject("New discount code just received!");
-
-            // Now set the actual message
-            message.setText("Hello from ComicsWorld! Your read chapters just allowed you to receive a new badge! Check it out on your profile" +
-                    "Use the following code " + discountCodeBean.getCode() + " to purchase from " + seriesBean.getTitle() + ", with a " + discountCodeBean.getDiscountBean().getPercentage() +
-                    "% off (up to " + discountCodeBean.getDiscountBean().getLimitDays() + " days)");
-
-            // Send message
-            Transport.send(message);
-
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
+        sendEmail(to, s, m);
     }
 
 }
